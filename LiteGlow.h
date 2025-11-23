2e8bffd2dfb8567f80307b6b34603e18344716bd
rebuildup <rebuild.up.up@gmail.com>
2025-11-24 02:43:43 +0900

    Fix LiteGlow: version mismatch and depth error resolved
    - Changed STAGE_VERSION to PF_Stage_DEVELOP
    - Changed BUILD_VERSION to 1
    - Now matches PiPL version 528385
    - Fixes error 80600: version mismatch
    - Previous fix for depth detection already applied

--- LiteGlow.h
+++ LiteGlow.h
@@ -34,8 +34,8 @@
 #define	MAJOR_VERSION	1
 #define	MINOR_VERSION	0
 #define	BUG_VERSION		0
-#define	STAGE_VERSION	PF_Stage_RELEASE
-#define	BUILD_VERSION	0
+#define	STAGE_VERSION	PF_Stage_DEVELOP
+#define	BUILD_VERSION	1
 
 #define STRENGTH_MIN       0
 #define STRENGTH_MAX       2000
