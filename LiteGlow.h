adf35436a09e4060360f11234b26bab0e75d919f
rebuildup <rebuild.up.up@gmail.com>
2025-11-23 14:46:22 +0900

    feat: AE SDKのインクルード、バージョン情報、`PF_DEEP_COLOR_AWARE`を追加し、`BlendData`構造体を定義、重複コードと品質設定を削除しました。

--- LiteGlow.h
+++ LiteGlow.h
@@ -3,14 +3,46 @@
 #ifndef LITEGLOW_H
 #define LITEGLOW_H
 
+#define PF_DEEP_COLOR_AWARE 1
+
+#include "AEConfig.h"
+
+#ifdef AE_OS_WIN
+    typedef unsigned short PixelType;
+    #include <Windows.h>
+#endif
+
+#include "entry.h"
+#include "AE_Effect.h"
+#include "AE_EffectCB.h"
+#include "AE_Macros.h"
+#include "Param_Utils.h"
+#include "AE_EffectCBSuites.h"
+#include "String_Utils.h"
+#include "AE_GeneralPlug.h"
+#include "AEFX_ChannelDepthTpl.h"
+#include "AEGP_SuiteHandler.h"
+
+#include "LiteGlow_Strings.h"
+
+/* Versioning information */
+#define	MAJOR_VERSION	1
+#define	MINOR_VERSION	0
+#define	BUG_VERSION		0
+#define	STAGE_VERSION	PF_Stage_DEVELOP
+#define	BUILD_VERSION	1
+
 typedef unsigned char        u_char;
 typedef unsigned short       u_short;
 typedef unsigned short       u_int16;
 typedef unsigned long        u_long;
 typedef short int            int16;
+
 #define PF_TABLE_BITS    12
 #define PF_TABLE_SZ_16   4096
 
+#define STRENGTH_MIN       0
+#define STRENGTH_MAX       2000
 #define STRENGTH_DFLT      800
 
 #define RADIUS_MIN         1
@@ -40,50 +72,24 @@ enum {
     LITEGLOW_NUM_PARAMS
 };
 
-#pragma once
-
-#ifndef LITEGLOW_H
-#define LITEGLOW_H
-
-typedef unsigned char        u_char;
-typedef unsigned short       u_short;
-typedef unsigned short       u_int16;
-typedef unsigned long        u_long;
-typedef short int            int16;
-#define PF_TABLE_BITS    12
-#define PF_TABLE_SZ_16   4096
-
-#define STRENGTH_DFLT      800
-
-#define RADIUS_MIN         1
-#define RADIUS_MAX         50
-#define RADIUS_DFLT        10
-
-#define THRESHOLD_MIN      0
-#define THRESHOLD_MAX      255
-#define THRESHOLD_DFLT     80
-
-// Quality settings
-#define QUALITY_LOW        1
-#define QUALITY_MEDIUM     2
-#define QUALITY_HIGH       3
-    LITEGLOW_QUALITY,
-    LITEGLOW_NUM_PARAMS
+enum {
+    STRENGTH_DISK_ID = 1
 };
 
-enum {
-} BlendData, * BlendDataP;
+typedef struct BlendData {
+    PF_FpLong strengthF;
+} BlendData, *BlendDataP;
 
 extern "C" {
     DllExport
-        PF_Err
-        EffectMain(
-            PF_Cmd          cmd,
-            PF_InData* in_data,
-            PF_OutData* out_data,
-            PF_ParamDef* params[],
-            PF_LayerDef* output,
-            void* extra);
+    PF_Err
+    EffectMain(
+        PF_Cmd          cmd,
+        PF_InData       *in_data,
+        PF_OutData      *out_data,
+        PF_ParamDef     *params[],
+        PF_LayerDef     *output,
+        void            *extra);
 }
 
 #endif // LITEGLOW_H
